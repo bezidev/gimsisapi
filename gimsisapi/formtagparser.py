5a9cd7c32b11cb97d1429f8e633c73f7e3a5d990
mytja <mytja@protonmail.com>
2022-10-01 12:19:45 +0200

    Nadomeščanja in vpisan dnevniški zapis

--- gimsisapi/formtagparser.py
+++ gimsisapi/formtagparser.py
@@ -10,7 +10,7 @@ def get_tags(text):
     return m
 
 class GimSisUra:
-    def __init__(self, ura, dan, ime, kratko_ime, razred, profesor, ucilnica):
+    def __init__(self, ura, dan, ime, kratko_ime, razred, profesor, ucilnica, dnevniski_zapis, vpisano_nadomescanje):
         self.ura = ura
         self.dan = dan
         self.ime = ime
@@ -18,11 +18,23 @@ class GimSisUra:
         self.razred = razred
         self.profesor = profesor
         self.ucilnica = ucilnica
+        self.dnevniski_zapis = dnevniski_zapis
+        self.vpisano_nadomescanje = vpisano_nadomescanje
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f"GimSisUra({self.ura}, {self.dan}, {self.ime}, {self.kratko_ime}, {self.razred}, {self.profesor}, {self.ucilnica}, {self.dnevniski_zapis}, {self.vpisano_nadomescanje})"
 
 def get_class(text):
     soup = BeautifulSoup(text, "html.parser")
     m = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}}
     for i in soup.find_all(id=re.compile("ctl00_ContentPlaceHolder1_wkgDnevnik_btnCell_.*")):
+        parent_classes = i.parent.attrs["class"]
+        dnevniski_zapis = "dzObstaja" in parent_classes
+        vpisano_nadomescanje = "flagS" in parent_classes
+
         kratko_ime = i.find("b").text
         id = i.attrs["id"].split("_")
         ura = int(id[4])
@@ -34,7 +46,7 @@ def get_class(text):
         profesor = title[3]
         ucilnica = title[4]
 
-        m[dan][ura] = GimSisUra(ura, dan, ime, kratko_ime, razred, profesor, ucilnica)
+        m[dan][ura] = GimSisUra(ura, dan, ime, kratko_ime, razred, profesor, ucilnica, dnevniski_zapis, vpisano_nadomescanje)
 
     return m
 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import asyncio
 
-from gimsisapi import GimSisAPI, make_markdown_table
+from gimsisapi import GimSisAPI
 
 gimsis = GimSisAPI(os.environ.get("GIMSIS_USERNAME"), os.environ.get("GIMSIS_PASSWORD"))
 
 async def main():
     classes, days = await gimsis.fetch_timetable()
-    print(make_markdown_table(classes, days))
+    print(classes, days)
     #await gimsis.fetch_timetable("12 09 2022")
 
 asyncio.run(main())
